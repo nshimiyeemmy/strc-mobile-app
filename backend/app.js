cc82376a7bfc7b7eeaeb96e2dd6c0862266cf383
nshimiye <nshimiyee311@gmail.com>
2022-02-07 18:56:53 +0200

    added user routes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+const errorMiddlewares = require('./middlewares/errors.js');
+
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,10 +12,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Importing all the routes
 
-// const users = require('./routes/userAuth');
+const users = require('./routes/userAuth');
 
 //doing routing
 
-// app.use('/api/v1', users);
+app.use('/api/v1', users);
+
+//Implementing the middleware to handle errors
+app.use(errorMiddlewares);
 
 module.exports = app;
