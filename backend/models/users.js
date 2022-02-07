2e761e6129b8707fef60c7f1169fd2634cd67868
nshimiye <nshimiyee311@gmail.com>
2022-02-07 09:31:05 +0200

    added user entity schema Info

---
+++ backend/models/users.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: [true, 'Please enter your first name'],
+    maxlength: [30, 'Your first name can not exceed 30 characters'],
+    validate: [validator.isAlpha, 'Please enter only String values']
+  },
+  lastname: {
+    type: String,
+    required: [true, 'Please enter your last name'],
+    maxlength: [30, 'Your last name can not exceed 30 characters'],
+    validate: [validator.isAlpha, 'Please enter only String values']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please enter your email'],
+    unique: true,
+    validate: [validator.isEmail, 'Please enter valid email address'],
+  },
+  
+  bio: {
+    type: String,
+    required: [true, 'Please tell us more about you'],
+    minlength: [10, 'Your bio should not be less than 10 characters'],
+    maxlength: [50, 'Your bio should not be more than 50 character'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter your Password'],
+    minlength: [6, 'Your password must be more than 6 characters'],
+    select: false,
+  },
+  avatar: {
+      //since I will use online cloud storage for images, I will need to store url for img
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: 'member',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+module.exports = mongoose.model('Users', userSchema);
