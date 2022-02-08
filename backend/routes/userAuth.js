6fe2164034149f4bc126a9afc5388c3275c9f1b9
nshimiye <nshimiyee311@gmail.com>
2022-02-08 06:37:18 +0200

    admin route to get user details

--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const {
     registerUser,
-    getAllUsers
+    getAllUsers,
+    getUserDetails
   } = require('../controllers/usersAuthController');
 
   router.route('/register').post(registerUser);
   router.route('/admin/users').get(getAllUsers);
+  router.route('/admin/user/:id').get(getUserDetails);
+
 
   module.exports = router;
