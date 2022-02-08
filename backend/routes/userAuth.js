3d854d5320c5c248619127926059874eba7a5c08
nshimiye <nshimiyee311@gmail.com>
2022-02-08 06:00:04 +0200

    added admin route to get all users

--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
     registerUser,
+    getAllUsers
   } = require('../controllers/usersAuthController');
 
   router.route('/register').post(registerUser);
+  router.route('/admin/users').get(getAllUsers);
 
   module.exports = router;
